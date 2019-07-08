d1c9c20ffacfb99f9cba5a47329bcb393a7a853e
githumbi <githumbi74@gmail.com>
2019-07-08 18:26:18 +0300

    added NEW,CREATE route

--- RESTful-Routes/blog-site/app.js
+++ RESTful-Routes/blog-site/app.js
@@ -1,13 +1,14 @@
 const express = require("express"),
-        faker = require("faker"),
-      mongoose = require("mongoose"),
-      bodyParser = require("body-parser")
-      app = express()
+    faker = require("faker"),
+    mongoose = require("mongoose"),
+    bodyParser = require("body-parser"),
+    // icons = require("evil-icons"),
+    app = express()
 
 mongoose.connect("mongodb://localhost/blog_db")
 app.set("view engine", "ejs")
 app.use(express.static("public"))
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 
 //CONFIG
@@ -16,7 +17,7 @@ var blogSchema = new mongoose.Schema({
     title: String,
     image: String,
     body: String,
-    created: {type: Date, default: Date.now}
+    created: { type: Date, default: Date.now }
 })
 
 Blog = mongoose.model("Blog", blogSchema)
@@ -28,30 +29,50 @@ Blog = mongoose.model("Blog", blogSchema)
 // })
 
 //fake data form FAKER :)
-Blog.create({
-    title: faker.name.title(),
-    body: faker.lorem.sentences(),
-    image: faker.image.imageUrl()
-  })
+
+// Blog.create({
+//     title: faker.name.title(),
+//     body: faker.lorem.paragraph(),
+//     image: faker.image.imageUrl()
+// })
+
 
 //RESTFUL Routes
-app.get("/", (req,res)=>{
+app.get("/", (req, res) => {
     res.redirect("/blogs")
 })
 
-
-app.get("/blogs", (req,res)=>{
-    Blog.find({}, (err, blogs)=>{
+//INEDX ROUTE
+app.get("/blogs", (req, res) => {
+    Blog.find({}, (err, blogs) => {
         if (err) {
             console.log("didint work")
         } else {
-            res.render("index", {blogs: blogs})
+            res.render("index", { blogs: blogs })
         }
     })
-    
+
 })
 
-app.listen(3000,()=>{
+//CREATE ROUTE
+app.get("/blogs/new", (req, res) => {
+    res.render("new")
+})
+
+//POST ROUTE
+app.post("/blogs", (req, res) => {
+    //create
+    Blog.create(req.body.blog, (err, newBlog) => {
+        if (err) {
+            res.render("new")
+        } else {
+            //redirect
+            res.redirect("/blogs")
+        }
+    })
+
+})
+
+app.listen(3000, () => {
     console.log("listening to port 3000")
 })
-
